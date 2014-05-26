04d1217d35b3201019f1f9adffb493ed970cab45
Oleg Polyakov <opolyo01@yahoo.com>
2014-05-25 20:14:27 -0700

    playoff games submission

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,24 @@ var groups = {
 	"G": ["GER", "USA", "POR", "GHA"],
 	"H": ["BEL", "KOR", "ALG", "RUS"]
 };
+var allTeams = ["BRA", "CRO", "CAM", "MEX","SPA", "NET", "CHI", "AUS",
+	"COL", "JAP", "GRE", "COT", "URU", "ITA", "COS", "ENG", "SWI", "HON", 
+	"ECU", "FRA", "ARG", "NIG", "BOS", "IRA", "GER", "USA", "POR", "GHA",
+	"BEL", "KOR", "ALG", "RUS"];
+	
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 	$.fn.editable.defaults.mode = 'inline';
+	$.fn.editable.defaults.showbuttons = false;
+	allTeams.unshift("Select");
+	_.each(groups, function(group){
+		group.unshift("Select");
+	});
 }
 
+
 $(".winner").editable({
+	source: allTeams,
+    type: "select",
 	title: "Winner"
 });
 
@@ -125,13 +138,44 @@ $.get("/findAllUserScores", {}, function(scores) {
 });
 
 $(".breadcrumb").on("click", function(evt){
+	var target = evt.target;
+	if($(target).hasClass('active')){
+		return;
+	}
 	$(".breadcrumb .active").removeClass('active');
-	$(this).addClass("active");
+	$(target).addClass("active");
 	$(".playoffs-content").toggle();
 	$(".group-games-content").toggle();
 });
 
-$(".submitResults").on("click", function(){
+$(".submitResults.playoffGames").on("click", function(){
+	var quaterOne = $("#quaterOne").html(),
+		quarterTwo = $("#quarterTwo").html(),
+		quarterThree = $("#quarterThree").html(),
+		quarterFour = $("#quarterFour").html(),
+		quarterFive = $("#quarterFive").html(),
+		quarterSix = $("#quarterSix").html(),
+		quarterSeven = $("#quarterSeven").html(),
+		quarterEight = $("#quarterEight").html(),
+		semiOne = $("#semiOne").html(),
+		semiTwo = $("#semiTwo").html(),
+		semiThree = $("#semiThree").html(),
+		semiFour = $("#semiFour").html(),
+		finalOne = $("#finalOne").html(),
+		finalTwo = $("#finalTwo").html(),
+		champion = $("#champion").html();
+	var resp = {
+		"quarterfinals": [quaterOne, quarterTwo, quarterThree, 
+						quarterFour, quarterFive, quarterSix,quarterSeven,quarterEight],
+		"semifinals": [semiOne, semiTwo, semiThree, semiFour],
+		"finals": [finalOne, finalTwo],
+		"champion": champion
+	};
+	console.log(resp);
+});
+
+
+$(".submitResults.groupGames").on("click", function(){
 	var arr = [], hs = {};
 	var scoreOne, scoreTwo;
 	var scores = $(".score");
