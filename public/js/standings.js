ae5046be10bd51f70c7cb399f1a862f583974434
Oleg Polyakov <opolyo01@yahoo.com>
2014-05-27 15:05:29 -0700

    added different view into standings

--- public/js/standings.js
+++ public/js/standings.js
@@ -1,29 +1,27 @@
 (function(){
+	var users = [];
+	var allUserNames = [];
+	Handlebars.registerHelper('gameTeamOne', function(gameId) {
+		 return _.find(games, function(game){return game.gameId == gameId;}).teamOne;
+	});
 	
+	Handlebars.registerHelper('gameTeamTwo', function(gameId) {
+		 return _.find(games, function(game){return game.gameId == gameId;}).teamTwo;
+	});
+	
+	Handlebars.registerHelper('formatTeam', function(name) {
+		 return name.substring(0, 3).toUpperCase();
+	});
+	
+	Handlebars.registerHelper('evenOdd', function(idx) {
+		 return idx % 2 === 0 ? "even" : "odd";
+	});
 	init();
 	
 	function init(){
 		$(".allUsers").hide();
-		perGame();
-		perUser();
-	}
-	
-	function perUser(){
-		$('.collapse').collapse();
-		var source = $("#allUsersTemplate").html();
-		var template = Handlebars.compile(source);
-		
-		Handlebars.registerHelper('gameTeamOne', function(gameId) {
-			 return _.find(games, function(game){return game.gameId == gameId;}).teamOne;
-		});
-		
-		Handlebars.registerHelper('gameTeamTwo', function(gameId) {
-			 return _.find(games, function(game){return game.gameId == gameId;}).teamTwo;
-		});
-		
 		$.get("/findAllUserScoresNoId", {}, function(scores) {
-			var allUserNames = [];
-			var users = []; 
+			
 			_.each(scores, function(score){allUserNames.push(score.user.username);});
 			allUserNames = _.uniq(allUserNames);
 			
@@ -35,16 +33,24 @@
 				});
 				users.push({"username": username, "scores": userScores});
 			});
-			
-			var html = template({"users": users});
-			$(".allUsers").html(html);
+			perGame();
+			perUser();
 		});
+		
+	}
+	
+	function perUser(){
+		$('.collapse').collapse();
+		var source = $("#allUsersTemplate").html();
+		var template = Handlebars.compile(source);
+		var html = template({"users": users});
+		$(".allUsers").html(html);
 	}
 	
 	function perGame(){
 		var source = $("#allGamesTemplate").html();
 		var template = Handlebars.compile(source);
-		var html = template({"games": []});
+		var html = template({"games": games, "users": users});
 		$(".allGames").html(html);
 	}
 	
